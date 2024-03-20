a0ca53f9f13a2fc6fd7dbeacf9e0e621baba62d5
C0D8 <matheusaguiar362@gmail.com>
2024-03-20 11:39:14 -0300

    .

--- src/main/java/insper/store/account/AccountController.java
+++ src/main/java/insper/store/account/AccountController.java
@@ -1,10 +1,12 @@
 package insper.store.account;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.cloud.openfeign.FeignClient;
 
 
@@ -26,4 +28,12 @@ public interface AccountController {
     public ResponseEntity<AccountOut> auth(
         @RequestBody(required = true) AuthIn in
     );    
+
+   @GetMapping("/accounts")
+    public ResponseEntity<AccountOut> read(
+        @RequestHeader(required = true, name = "id-user") String idUser,
+        @RequestHeader(required = true, name = "role-user") String roleUser
+    );
+
+    
 }
